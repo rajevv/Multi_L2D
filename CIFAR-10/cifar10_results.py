d5d0e40e9dd18060f9d4303d55f9cebe15f0b52a
Dani Barrejon <dbarrejo@ing.uc3m.es>
2022-05-25 19:43:13 +0200

    Add final results for CIFAR10.

--- CIFAR-10/cifar10_results.py
+++ CIFAR-10/cifar10_results.py
@@ -45,30 +45,30 @@ plot_args = {"linestyle": "-",
              }
 
 # ====== Experiment 1: Increase number of experts ======
-# n_experts = [1, 2, 4, 6, 8]
-# # Percentage
-# ece_softmax = np.array([0.04212178, 0.03236192, 0.04614954, 0.048385993, 0.046213374]) * 100
-# ece_ova = np.array([0.13224682, 0.12023479, 0.100782335, 0.095202245, 0.10850768]) * 100
-#
-# sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 30})
-# # sns.set_style("whitegrid")
-# f, ax = plt.subplots(1, 1)
-# ax.plot(n_experts, ece_softmax, label=r"Softmax", **plot_args)
-# ax.plot(n_experts, ece_ova, label=r"OvA", **plot_args)
-# plt.xticks(n_experts, n_experts)
-# plt.ylabel(r'Average ECE $\%$')
-# plt.xlabel(r'Number of Experts')
-# plt.legend()
-# plt.grid()
-# plt.tight_layout()
-# plt.savefig("cifar_increase_experts.pdf")
+n_experts = [1, 2, 4, 6, 8]
+# Percentage
+ece_softmax = np.array([0.04212178, 0.03236192, 0.04614954, 0.048385993, 0.046213374]) * 100
+ece_ova = np.array([0.033160426, 0.027528169, 0.029059665, 0.028813586, 0.03282707]) * 100
 
-#
+sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 30})
+# sns.set_style("whitegrid")
+f, ax = plt.subplots(1, 1)
+ax.plot(n_experts, ece_softmax, label=r"Softmax", **plot_args)
+ax.plot(n_experts, ece_ova, label=r"OvA", **plot_args)
+plt.xticks(n_experts, n_experts)
+plt.ylabel(r'Average ECE $\%$')
+plt.xlabel(r'Number of Experts')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.savefig("cifar_increase_experts.pdf")
+
+
 # # ====== Experiment 2: Increase confidence for 3 experts ======
 p_experts = [0.2, 0.4, 0.6, 0.8, 0.95]
 # Percentage
 ece_softmax = np.array([0.20560052, 0.3304271, 0.43687683, 0.5407775, 0.5562724]) * 100
-ece_ova = np.array([0.08306407, 0.09608422, 0.08810554, 0.092499636, 0.09254267]) * 100
+ece_ova = np.array([0.007408993, 0.009338078, 0.009841489, 0.008472728, 0.011463852]) * 100
 
 sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 30})
 
