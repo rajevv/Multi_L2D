Fix bugs on OvA. Take sigmoid, not softmax.

CIFAR-10/test_increase_confidence.py
@@ -92,16 +92,10 @@ for p in p_experts:
     true.append(true_label['test'])
     exps.append(exp_pred['test'])
     c = torch.tensor(conf['test'])
-    print(c.shape)
     # DANI Correction ===
-    c = c.softmax(dim=1)
+    c = c.sigmoid()
     # DANI Correction ===
-
-    temp = 0
-    for i in range(n_experts):
-        temp += c[:, (n_classes + n_experts) - (i + 1)]
-    prob = c / (1.0 - temp).unsqueeze(-1)
-    confs.append(prob)
+    confs.append(c)
 
 ECEs = []
 for i in range(len(p_experts)):

CIFAR-10/test_increase_experts.py
@@ -23,7 +23,6 @@ for n in n_experts:
     true.append(true_label['test'])
     exps.append(exp_pred['test'])
     c = torch.tensor(conf['test'])
-    print(c.shape)
     # DANI Correction ===
     c = c.softmax(dim=1)
     # DANI Correction ===
@@ -80,14 +79,9 @@ for n in n_experts:
     c = torch.tensor(conf['test'])
     print(c.shape)
     # DANI Correction ===
-    c = c.softmax(dim=1)
+    c = c.sigmoid()
     # DANI Correction ===
-
-    temp = 0
-    for i in range(n):
-        temp += c[:, (n_classes + n) - (i + 1)]
-    prob = c / (1.0 - temp).unsqueeze(-1)
-    confs.append(prob)
+    confs.append(c)
 
 ECEs = []
 for i in range(len(n_experts)):
@@ -113,4 +107,3 @@ for l in ECEs:
 print("OvA Increasing # Experts ECE: {}".format(Y))
 plt.plot(Y)
 plt.show()
-