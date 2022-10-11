93a3ecb75abb0fc0bacc1066b0af2f02cf5b5beb
dbarrejon <dbarrejo@ing.uc3m.es>
2022-10-12 00:47:18 +0200

    Update conformal_nonrandomized_oracles.py

--- CIFAR-10/conformal_results/conformal_nonrandomized_oracles.py
+++ CIFAR-10/conformal_results/conformal_nonrandomized_oracles.py
@@ -1,12 +1,16 @@
+
 import os
 import sys
 
 sys.path.append("../")  # append for conformal function
-from conformal import conformal
+from conformal import conformal, utils
 from conformal.conformal_plots import plot_metric, compare_metric
 
 # Experiment params ==============
 # *** Change from here for other exps ***
+
+conformal_type = "naive"
+
 experiment_name = "increase_oracle_v2"
 experiment_args = {"n_experts": 10,
                    "n_classes": 10,
@@ -16,38 +20,48 @@ exp_list = [1, 3, 5, 7, 9]
 seeds = [436, 625, 948]
 seed_name = "seed_{}"
 model_name = "_k_{}"  # to include values in exp_list
-# *** Change from here for other exps ***
+
+# Plot params
+xlabel = "Oracles"
+
+# Experiment params ==============
 
 # Ensemble methods ==============
 metric_methods = ["standard",  # standard L2D
                   "last", "random", "voting",  # conformal-based
                   "ensemble"]  # basic fixed-size ensemble
 
+# Metric depending on conformal type
+metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "qhat"]
+if conformal_type == "regularized":
+    metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "lamhat"]
+
 # Conformal params ==============
 alpha = 0.1
 cal_percent = 0.8
 
-results_path = "../results/{}/".format(experiment_name)
+results_path = "../results/{}/{}/".format(experiment_name, conformal_type)
 if not os.path.exists(results_path):
     os.makedirs(results_path)
 
 ova_fig_path = "{}_ova_".format(experiment_name)
-plot_args_ova = {"xlabel": "Oracles",
+plot_args_ova = {"xlabel": xlabel,
                  "title": "CIFAR-10 OvA",
                  "fig_path": results_path + ova_fig_path + "{}.pdf"}
 
 softmax_fig_path = "{}_softmax_".format(experiment_name)
-plot_args_softmax = {"xlabel": "Oracles",
+plot_args_softmax = {"xlabel": xlabel,
                      "title": "CIFAR-10 Softmax",
                      "fig_path": results_path + softmax_fig_path + "{}.pdf"}
 
 compare_fig_path = "{}_".format(experiment_name)
-plot_args = {"xlabel": "Oracles",
+plot_args = {"xlabel": xlabel,
              "title": "CIFAR-10",
-             "fig_path": results_path + compare_fig_path + "{}.pdf"}
+             "fig_path": results_path + compare_fig_path + "{}.pdf",
+             "metric_path": results_path + compare_fig_path + "{}_{}.txt"}
 # =================================
-# Experiment params ==============
-
+# FROM HERE, SAME FOR ALL EXPERIMENTS
+# =================================
 # =========== #
 # === OvA === #
 # =========== #
@@ -61,11 +75,12 @@ ova_results = conformal.load_results(path_confidence_ova, path_experts_ova, path
                                      seeds, exp_list, method="ova")
 
 # Process Results ===
-ova_metrics = conformal.process_conformal_results(ova_results, exp_list, experiment_args, cal_percent=cal_percent,
-                                                  alpha=alpha, metric_methods=metric_methods)
-
-metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "qhat"]
+ova_metrics = conformal.process_conformal_results(ova_results, exp_list, experiment_args,
+                                                  cal_percent=cal_percent,
+                                                  alpha=alpha, metric_methods=metric_methods,
+                                                  conformal_type=conformal_type)
 for met in metrics:
+    utils.save_metric(ova_metrics, met, metric_methods, plot_args["metric_path"].format(met, "ova"))
     f, ax = plot_metric(ova_metrics, metric_methods, met, plot_args_ova)
 
 # =============== #
@@ -82,15 +97,14 @@ softmax_results = conformal.load_results(path_confidence_softmax, path_experts_s
 # Process Results ===
 softmax_metrics = conformal.process_conformal_results(softmax_results, exp_list, experiment_args,
                                                       cal_percent=cal_percent,
-                                                      alpha=alpha, metric_methods=metric_methods)
-
-metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "qhat"]
+                                                      alpha=alpha, metric_methods=metric_methods,
+                                                      conformal_type=conformal_type)
 for met in metrics:
+    utils.save_metric(softmax_metrics, met, metric_methods, plot_args["metric_path"].format(met, "softmax"))
     f, ax = plot_metric(softmax_metrics, metric_methods, met, plot_args_softmax)
 
 # ======================= #
 # === Compare results === #
 # ======================= #
-metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "qhat"]
 for met in metrics:
     f, ax = compare_metric(ova_metrics, softmax_metrics, metric_methods, met, plot_args)
