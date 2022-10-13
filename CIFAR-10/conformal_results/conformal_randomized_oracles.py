7575d89721844cbe4f35576e59e5f39ae61f365b
dbarrejon <dbarrejo@ing.uc3m.es>
2022-10-13 20:45:16 +0200

    Update for set size histograms

--- CIFAR-10/conformal_results/conformal_randomized_oracles.py
+++ CIFAR-10/conformal_results/conformal_randomized_oracles.py
@@ -1,15 +1,17 @@
-
 import os
 import sys
 
 sys.path.append("../")  # append for conformal function
 from conformal import conformal, utils
 from conformal.conformal_plots import plot_metric, compare_metric
-
+from collections import Counter
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 # Experiment params ==============
 # *** Change from here for other exps ***
 
-conformal_type = "regularized"
+conformal_type = "naive"
 
 experiment_name = "increase_oracle"
 experiment_args = {"n_experts": 10,
@@ -17,6 +19,7 @@ experiment_args = {"n_experts": 10,
                    "ensemble_size": 5}
 exp_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 seeds = [436, 791, 948, 1750]
+seeds = [436]
 seed_name = "seed_{}"
 model_name = "_k_{}"  # to include values in exp_list
 
@@ -29,9 +32,11 @@ xlabel = "Oracles"
 metric_methods = ["standard",  # standard L2D
                   "last", "random", "voting",  # conformal-based
                   "ensemble"]  # basic fixed-size ensemble
-
+metric_methods = ["voting"]  # b
 # Metric depending on conformal type
 metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "qhat"]
+metrics = ["set_sizes"]
+
 if conformal_type == "regularized":
     metrics = ["system_accuracy", "expert_accuracy", "coverage_test", "avg_set_size", "lamhat"]
 
@@ -78,9 +83,33 @@ ova_metrics = conformal.process_conformal_results(ova_results, exp_list, experim
                                                   cal_percent=cal_percent,
                                                   alpha=alpha, metric_methods=metric_methods,
                                                   conformal_type=conformal_type)
+
+
+set_sizes = np.zeros((experiment_args["n_experts"], len(exp_list)))
+for seed in seeds:
+    for i, exp in enumerate(exp_list):
+        set_size = np.array(ova_metrics[seed][exp]["voting"]["set_sizes"][0])
+        counter = Counter(set_size)
+        idx = list(counter.keys())
+        idx = [j - 1 for j in idx]
+        counts = list(counter.values())
+        set_sizes[idx, i] += counts
+
+max_col = np.max(set_sizes, 0)
+renorm_by_col = np.tile(max_col, (10, 1))
+set_sizes_renorm = set_sizes/renorm_by_col
+f, ax = plt.subplots(1, 1)
+sns.heatmap(set_sizes_renorm, cmap="viridis")
+ax.set_xticks(np.array(exp_list)+0.5, exp_list)
+ax.set_yticks(np.arange(1,11) - 0.5, np.arange(1,11))
+ax.set_ylabel("Set sizes")
+ax.set_xlabel("Oracles")
+plt.title("{}".format(conformal_type))
+
 for met in metrics:
     utils.save_metric(ova_metrics, met, metric_methods, plot_args["metric_path"].format(met, "ova"))
     f, ax = plot_metric(ova_metrics, metric_methods, met, plot_args_ova)
+
 
 # =============== #
 # === Softmax === #
@@ -107,4 +136,3 @@ for met in metrics:
 # ======================= #
 for met in metrics:
     f, ax = compare_metric(ova_metrics, softmax_metrics, metric_methods, met, plot_args)
-
